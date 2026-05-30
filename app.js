d0a8e06902ec8c652757cb47a349e0024833fb4a
이지연 <writemumu@naver.com>
2026-05-30 20:51:01 +0900

    fix: add /internal/auth route for gateway routing

--- app.js
+++ app.js
@@ -74,6 +74,7 @@ app.use("/api/visions", require("./routes/visionRouter"));
 app.use("/api/select", require("./routes/select"));
 app.use("/api/preferences", require("./routes/preferencesRoutes"));
 app.use("/internal", require("./routes/internalRouter"));
+app.use("/internal/auth", require("./routes/authRouter")); //Gateway용 추가
 
 app.use((err, req, res, next) => {
     if (err.code === "EBADCSRFTOKEN") {
